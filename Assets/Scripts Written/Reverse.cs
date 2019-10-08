172c30082e2970e938033c5aa89a38d8748572db
Arushi Somani <arushisomani@berkeley.edu>
2019-10-08 09:24:47 -0700

    adding vr component

---
+++ Assets/Scripts Written/Reverse.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reverse: MonoBehaviour 
+{
+
+	public GameObject SferaPanoramica;
+
+	void Awake()
+	{
+
+		InvertSphere();
+	}
+
+	void InvertSphere()
+	{
+		Vector3[] normals = SferaPanoramica.GetComponent<MeshFilter>().mesh.normals;
+		for(int i = 0; i < normals.Length; i++)
+		{
+			normals[i] = -normals[i];
+		}
+		SferaPanoramica.GetComponent<MeshFilter>().mesh.normals = normals;
+
+		int[] triangles = SferaPanoramica.GetComponent<MeshFilter>().mesh.triangles;
+
+		for (int i = 0; i < triangles.Length; i+=3)
+		{
+			int t = triangles[i];
+			triangles[i] = triangles[i + 2];
+			triangles[i + 2] = t;
+
+		}           
+
+		SferaPanoramica.GetComponent<MeshFilter>().mesh.triangles= triangles;
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
